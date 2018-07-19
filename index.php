7ad097eb3d0b80067016c59692caca5a32f054ef
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-07-19 16:25:09 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 //traz as dependencias do projeto
 require_once("vendor/autoload.php");
 
@@ -7,6 +9,7 @@ require_once("vendor/autoload.php");
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 //criando as ROTAS da aplicação
 $app = new Slim();
@@ -24,10 +27,39 @@ $app->get('/', function() {
 
 $app->get('/admin', function() {
 
+    User::verifyLogin();
+    
     $page = new PageAdmin();
     
     $page ->setTpl("index");
     
+});
+
+$app->get('/admin/login', function() {
+
+    $page = new PageAdmin([
+        "header"=>false,
+        "footer"=>false
+    ]);
+    
+    $page ->setTpl("login");
+    
+});
+
+$app->post('/admin/login', function() {
+
+    User::login($_POST["login"], $_POST["password"]);
+    
+    header("Location: /admin");
+    
+    exit();
+});
+
+$app->get('/admin/logout', function() {
+
+    User::logout();
+    header("Location: /admin/login");
+    exit();
 });
 
 
