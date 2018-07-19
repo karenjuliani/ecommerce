88ce6ae6cb266920e805af55dfb272129887650c
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-07-19 09:36:23 -0300

    autoload da classe sql feita local, sem importar

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit10510da9c5e1d00028288dd5776148b3
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/karenjuliani/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInit10510da9c5e1d00028288dd5776148b3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInit10510da9c5e1d00028288dd5776148b3::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit10510da9c5e1d00028288dd5776148b3::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit10510da9c5e1d00028288dd5776148b3::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit10510da9c5e1d00028288dd5776148b3::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit10510da9c5e1d00028288dd5776148b3::$classMap;
 
