c5eb407a13ebd76219efa98186b8cbaa935d5736
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-07-19 10:15:32 -0300

    usando templates (TPL) e carregando a classe Page

--- index.php
+++ index.php
@@ -1,19 +1,26 @@
-<?php 
+<?php
 
+//traz as dependencias do projeto
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+//declarando quais namespace usaremos
+use \Slim\Slim;
+use \Hcode\Page;
+
+//criando as ROTAS da aplicação
+$app = new Slim();
 
 $app->config('debug', true);
 
+//quando for chamado a pasta raiz será executado todo este bloco do $app->get('/',
 $app->get('/', function() {
+
+    $page = new Page();
     
-	$sql = new Hcode\DB\Sql();
-        $results = $sql ->select("SELECT * FROM tb_users");
-        
-        echo json_encode($results);
+    $page ->setTpl("index");
+    
 });
 
+//depois de tudo "carregado" ele executa
 $app->run();
-
- ?>
+?>
