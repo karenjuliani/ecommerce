114f61e42ce7e051983c29a21389f86c371521d0
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-07-20 16:29:38 -0300

    INSERT-UPDATE-DELETE de usuário

--- index.php
+++ index.php
@@ -6,10 +6,12 @@ session_start();
 require_once("vendor/autoload.php");
 
 //declarando quais namespace usaremos
-use \Slim\Slim;
-use \Hcode\Page;
-use \Hcode\PageAdmin;
-use \Hcode\Model\User;
+
+
+use Hcode\Model\User;
+use Hcode\Page;
+use Hcode\PageAdmin;
+use Slim\Slim;
 
 //criando as ROTAS da aplicação
 $app = new Slim();
@@ -20,38 +22,35 @@ $app->config('debug', true);
 $app->get('/', function() {
 
     $page = new Page();
-    
-    $page ->setTpl("index");
-    
+
+    $page->setTpl("index");
 });
 
 $app->get('/admin', function() {
 
     User::verifyLogin();
-    
+
     $page = new PageAdmin();
-    
-    $page ->setTpl("index");
-    
+
+    $page->setTpl("index");
 });
 
 $app->get('/admin/login', function() {
 
     $page = new PageAdmin([
-        "header"=>false,
-        "footer"=>false
+        "header" => false,
+        "footer" => false
     ]);
-    
-    $page ->setTpl("login");
-    
+
+    $page->setTpl("login");
 });
 
 $app->post('/admin/login', function() {
 
     User::login($_POST["login"], $_POST["password"]);
-    
+
     header("Location: /admin");
-    
+
     exit();
 });
 
@@ -61,6 +60,93 @@ $app->get('/admin/logout', function() {
     header("Location: /admin/login");
     exit();
 });
+
+// USUÁRIOS ------------------------------------------
+$app->get("/admin/users", function() {
+
+    User::verifyLogin();
+
+    $users = User::listAll();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users", array(
+        "users" => $users
+    ));
+});
+
+$app->get("/admin/users/create", function() {
+    User::verifyLogin();
+
+    $page = new PageAdmin();
+
+    $page->setTpl("users-create");
+});
+
+$app->get("/admin/users/:iduser/delete", function($iduser) {
+    User::verifyLogin();
+    $user = new User();
+    
+    $user->get((int) $iduser);
+    
+    $user->delete();
+    
+    header("Location: /admin/users");
+    
+    exit() ;
+});
+
+
+
+$app->get('/admin/users/:iduser', function($iduser) {
+    User::verifyLogin();
+    
+    $user = new User();
+    
+    $user->get((int) $iduser);
+    
+    $page = new PageAdmin();
+    
+    $page->setTpl("users-update", array(
+        "user" => $user->getValues()
+    ));
+});
+
+$app->post("/admin/users/create", function() {
+    User::verifyLogin();    
+    
+    $user = new User();
+    
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+    
+    $user->setData($_POST);
+    
+    
+    $user->save();
+    
+    header("Location: /admin/users");
+    
+    exit() ;
+});
+
+$app->post("/admin/users/:iduser", function($iduser) {
+    User::verifyLogin();
+    
+    $user = new User();
+    
+    $_POST["inadmin"] = (isset($_POST["inadmin"]))?1:0;
+    
+    $user->get((int)$iduser);
+    
+    $user->setData($_POST);
+    
+    $user->update();
+    
+    header("Location: /admin/users");
+    
+    exit() ;
+});
+
 
 
 //depois de tudo "carregado" ele executa
