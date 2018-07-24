56afcb9a1927fe3edf0fdcbe11a2769c68b8db45
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-07-24 16:33:28 -0300

    Site - Produtos

--- site.php
+++ site.php
@@ -1,12 +1,18 @@
 <?php
+
+use Hcode\Model\Product;
 use Hcode\Page;
 
 //quando for chamado a pasta raiz será executado todo este bloco do $app->get('/',
 $app->get('/', function() {
 
+    $products = Product::listAll();
+
     $page = new Page();
 
-    $page->setTpl("index");
+    $page->setTpl("index", [
+        'products' =>  Product::checkList($products)
+    ]);
 });
 
 
