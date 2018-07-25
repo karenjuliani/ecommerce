37c4ea436649f9408f12fde4fe4bdd4a07f210ab
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-07-25 10:42:15 -0300

    Paginação

--- site.php
+++ site.php
@@ -12,20 +12,34 @@ $app->get('/', function() {
     $page = new Page();
 
     $page->setTpl("index", [
-        'products' =>  Product::checkList($products)
+        'products' => Product::checkList($products)
     ]);
 });
 
 $app->get("/categories/:idcategory", function($idcategory) {
-    
+
+    $page = (isset($_GET['page'])) ? (int) $_GET['page'] : 1;
+
     $category = new Category();
 
     $category->get((int) $idcategory);
+
+    $pagination = $category->getProductsPage($page);
+
+    $pages = [];
+
+    for ($i = 1; $i <= $pagination['pages']; $i++) {
+        array_push($pages, [
+        'link' => '/categories/'.$category->getidcategory().'?page='.$i,
+        'page' => $i
+        ]);
+    }
 
     $page = new Page();
 
     $page->setTpl("category", [
         'category' => $category->getValues(),
-        'products' => Product::checkList($category->getProducts())
+        'products' => $pagination["data"],
+        'pages' => $pages
     ]);
 });
