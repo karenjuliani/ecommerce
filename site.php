c5130fd516f96511879c427ce1dde056a9a9aba9
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-08-01 10:29:06 -0300

    Minha conta

--- site.php
+++ site.php
@@ -250,7 +250,7 @@ $app->get("/forgot/reset", function() {
 
     $page = new Page();
 
-     $page->setTpl("forgot-reset", array(
+    $page->setTpl("forgot-reset", array(
         "name" => $user["desperson"],
         "code" => $_GET["code"]
     ));
@@ -274,6 +274,53 @@ $app->post("/forgot/reset", function() {
     $page->setTpl("forgot-reset-success");
 });
 
+$app->get("/profile", function() {
+    User::verifyLogin(false);
+    $user = User::getFromSession();
+    $page = new Page();
+    $page->setTpl("profile", [
+        'user' => $user->getValues(),
+        'profileMsg' => User::getSuccess(),
+        'profileError' => User::getError()
+    ]);
+});
 
+$app->post("/profile", function() {
+
+    User::verifyLogin(false);
+
+    if (!isset($_POST['desperson']) || $_POST['desperson'] === '') {
+        User::setError("Preencha o seu nome.");
+        header("Location: /profile");
+        exit();
+    }
+    if (!isset($_POST['desemail']) || $_POST['desemail'] === '') {
+        User::setError("Preencha o seu e-mail.");
+        header("Location: /profile");
+        exit();
+    }
+
+    $user = User::getFromSession();
+
+    if ($_POST['desemail'] !== $user->getdesemail()) {
+        if (User::checkLoginExist($_POST['desemail'])) {
+            User::setError("Este endereço de e-mail já está cadastrado.");
+            header("Location: /profile");
+            exit();
+        }
+    }
+
+   $_POST['iduser'] = $user->getiduser(); 
+    $_POST['inadmin'] = $user->getinadmin();
+    $_POST['despassword'] = $user->getdespassword();
+    $_POST['deslogin'] = $_POST['desemail'];
+    $user->setData($_POST);
+    $user->update(false);
+
+    $_SESSION[User::SESSION] = $user->getValues();
+    User::setSuccess("Dados alterados com sucesso!");
+    header("Location: /profile");
+    exit();
+});
 
 
