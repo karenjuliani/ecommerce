b23f80bc3a5d4c43a3c977c3528d53deffa2e453
ecode-03 <ecode-03@DESKTOP-CT87924>
2018-08-06 14:19:00 -0300

    Alterar a senha

--- site.php
+++ site.php
@@ -474,42 +474,103 @@ $app->get("/boleto/:idorder", function($idorder) {
     $dadosboleto["cedente"] = "HCODE TREINAMENTOS LTDA - ME";
 
 // NÃO ALTERAR!
-    $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include".DIRECTORY_SEPARATOR;
+    $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "res" . DIRECTORY_SEPARATOR . "boletophp" . DIRECTORY_SEPARATOR . "include" . DIRECTORY_SEPARATOR;
     require_once($path . "funcoes_itau.php");
     require_once($path . "layout_itau.php");
 });
 
-$app->get("/profile/orders", function(){
-    
+$app->get("/profile/orders", function() {
+
     User::verifyLogin(false);
-    
+
     $user = User::getFromSession();
-    
+
     $page = new Page();
-    $page->setTpl("profile-orders",[
-        'orders'=>$user->getOrders()
+    $page->setTpl("profile-orders", [
+        'orders' => $user->getOrders()
     ]);
 });
 
-$app->get("/profile/orders/:idorder", function($idOrder){
-    
+$app->get("/profile/orders/:idorder", function($idOrder) {
+
     User::verifyLogin(false);
-    
+
     $order = new Order();
-    
+
     $order->get((int) $idOrder);
-    
+
     $cart = new Cart();
-    
-    $cart->get((int)$order->getidcart());
-    
+
+    $cart->get((int) $order->getidcart());
+
     $cart->getCalculateTotal();
-    
+
     $page = new Page();
-    $page->setTpl("profile-orders-detail",[
-        'order'=>$order->getValues(),
-        'cart'=>$cart->getValues(),
-        'products'=>$cart->getProducts()
+    $page->setTpl("profile-orders-detail", [
+        'order' => $order->getValues(),
+        'cart' => $cart->getValues(),
+        'products' => $cart->getProducts()
     ]);
-    
+});
+
+$app->get("/profile/change-password", function() {
+    User::verifyLogin(false);
+
+    $page = new Page();
+
+    $page->setTpl("profile-change-password", [
+        'changePassError' => User::getError(),
+        'changePassSuccess' => User::getSuccess()
+    ]);
+});
+
+$app->post("/profile/change-password", function() {
+
+    User::verifyLogin(false);
+
+    if (!isset($_POST['current_pass']) || $_POST['current_pass'] === '') {
+        User::setError("Digite a senha atual.");
+        header("Location: /profile/change-password");
+        exit();
+    }
+
+    if (!isset($_POST['new_pass']) || $_POST['new_pass'] === '') {
+        User::setError("Digite a nova senha.");
+        header("Location: /profile/change-password");
+        exit();
+    }
+
+    if (!isset($_POST['new_pass_confirm']) || $_POST['new_pass_confirm'] === '') {
+        User::setError("Confirme a nova senha.");
+        header("Location: /profile/change-password");
+        exit();
+    }
+
+    if ($_POST['current_pass'] === $_POST['new_pass']) {
+        User::setError("Sua nova senha deve ser diferente da atual.");
+        header("Location: /profile/change-password");
+        exit();
+    }
+
+    if ($_POST['new_pass'] !== $_POST['new_pass_confirm']) {
+        User::setError("A nova senha é diferente de sua confirmação.");
+        header("Location: /profile/change-password");
+        exit();
+    }
+
+    $user = User::getFromSession();
+
+    if (!password_verify($_POST['current_pass'], $user->getdespassword())) {
+        User::setError("A senha está inválida.");
+        header("Location: /profile/change-password");
+        exit();
+    }
+
+    $user->setdespassword($_POST['new_pass']);
+    $user->update();
+
+    User::setSuccess("Senha alterada com sucesso!");
+
+    header("Location: /profile/change-password");
+    exit();
 });
